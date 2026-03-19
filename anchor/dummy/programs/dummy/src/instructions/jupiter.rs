055e42c7e30605fa0fa40a374d35a77d2eeda762
Chii Yuen <chii.yuen@hotmail.com>
2026-03-19 11:56:13 +0800

    feat: add v2 support

--- anchor/dummy/programs/dummy/src/instructions/jupiter.rs
+++ anchor/dummy/programs/dummy/src/instructions/jupiter.rs
@@ -9,6 +9,12 @@ pub const ROUTE_DISCRIMINATOR: [u8; 8] = [229, 23, 203, 151, 122, 227, 173, 42];
 pub const SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR: [u8; 8] =
     [176, 209, 105, 168, 154, 125, 69, 62];
 pub const SHARED_ACCOUNTS_ROUTE_DISCRIMINATOR: [u8; 8] = [193, 32, 155, 51, 65, 214, 156, 129];
+pub const EXACT_OUT_ROUTE_V2_DISCRIMINATOR: [u8; 8] = [157, 138, 184, 82, 21, 244, 243, 36];
+pub const ROUTE_V2_DISCRIMINATOR: [u8; 8] = [187, 100, 250, 204, 49, 196, 175, 20];
+pub const SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR: [u8; 8] =
+    [53, 96, 229, 202, 216, 187, 250, 24];
+pub const SHARED_ACCOUNTS_ROUTE_V2_DISCRIMINATOR: [u8; 8] =
+    [209, 152, 83, 147, 124, 254, 216, 233];
 
 pub struct Jupiter<'info> {
     pub token_program: AccountInfo<'info>,
@@ -27,26 +33,84 @@ impl<'info> Jupiter<'info> {
         amount: u64,
         slippage_bps: u16,
     ) -> Result<()> {
-        let swap_data_length = swap_data.len();
-        let bps_offset = swap_data_length - size_of::<u16>() - size_of::<u8>();
-        let amount_offset = bps_offset - size_of::<u64>() - size_of::<u64>();
+        if swap_data.len() < 8 {
+            return Err(Error::from(ProgramError::InvalidInstructionData));
+        }
 
-        require_eq!(
-            amount,
-            u64::from_le_bytes(
-                swap_data[amount_offset..amount_offset + size_of::<u64>()]
-                    .try_into()
-                    .unwrap()
-            )
-        );
-        require_eq!(
-            slippage_bps,
-            u16::from_le_bytes(
-                swap_data[bps_offset..bps_offset + size_of::<u16>()]
-                    .try_into()
-                    .unwrap()
-            )
-        );
+        let swap_data_length = swap_data.len();
+        let discriminator = &swap_data[..8];
+
+        if discriminator == EXACT_OUT_ROUTE_DISCRIMINATOR
+            || discriminator == ROUTE_DISCRIMINATOR
+            || discriminator == SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR
+            || discriminator == SHARED_ACCOUNTS_ROUTE_DISCRIMINATOR
+        {
+            let bps_offset = swap_data_length - size_of::<u16>() - size_of::<u8>();
+            let amount_offset = bps_offset - size_of::<u64>() - size_of::<u64>();
+
+            require_eq!(
+                amount,
+                u64::from_le_bytes(
+                    swap_data[amount_offset..amount_offset + size_of::<u64>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+            require_eq!(
+                slippage_bps,
+                u16::from_le_bytes(
+                    swap_data[bps_offset..bps_offset + size_of::<u16>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+        } else if discriminator == EXACT_OUT_ROUTE_V2_DISCRIMINATOR
+            || discriminator == ROUTE_V2_DISCRIMINATOR
+        {
+            let amount_offset = 8;
+            let slippage_bps_offset = amount_offset + size_of::<u64>() + size_of::<u64>();
+
+            require_eq!(
+                amount,
+                u64::from_le_bytes(
+                    swap_data[amount_offset..amount_offset + size_of::<u64>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+            require_eq!(
+                slippage_bps,
+                u16::from_le_bytes(
+                    swap_data[slippage_bps_offset..slippage_bps_offset + size_of::<u16>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+        } else if discriminator == SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR
+            || discriminator == SHARED_ACCOUNTS_ROUTE_V2_DISCRIMINATOR
+        {
+            let amount_offset = 8 + size_of::<u8>();
+            let slippage_bps_offset = amount_offset + size_of::<u64>() + size_of::<u64>();
+
+            require_eq!(
+                amount,
+                u64::from_le_bytes(
+                    swap_data[amount_offset..amount_offset + size_of::<u64>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+            require_eq!(
+                slippage_bps,
+                u16::from_le_bytes(
+                    swap_data[slippage_bps_offset..slippage_bps_offset + size_of::<u16>()]
+                        .try_into()
+                        .unwrap()
+                )
+            );
+        } else {
+            return Err(Error::from(ProgramError::InvalidInstructionData));
+        }
 
         Ok(())
     }
@@ -138,13 +202,21 @@ impl<'info> Jupiter<'info> {
             data if data.starts_with(&SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR)
                 || data.starts_with(&SHARED_ACCOUNTS_ROUTE_DISCRIMINATOR) =>
             {
+                if remaining_accounts.len() < 3 {
+                    return Err(Error::from(ProgramError::NotEnoughAccountKeys));
+                }
+
+                let program_authority = &remaining_accounts[0];
+                let program_source_token_account = &remaining_accounts[1];
+                let program_destination_token_account = &remaining_accounts[2];
+
                 let mut account_infos = vec![
                     self.token_program.to_account_info(),
-                    remaining_accounts[0].to_account_info(),
+                    program_authority.to_account_info(),
                     self.token_account_authority.to_account_info(),
                     self.source_token_account.to_account_info(),
-                    remaining_accounts[1].to_account_info(),
-                    remaining_accounts[2].to_account_info(),
+                    program_source_token_account.to_account_info(),
+                    program_destination_token_account.to_account_info(),
                     self.destination_token_account.to_account_info(),
                     self.source_mint.to_account_info(),
                     self.destination_mint.to_account_info(),
@@ -159,11 +231,11 @@ impl<'info> Jupiter<'info> {
 
                 let mut accounts = vec![
                     AccountMeta::new_readonly(self.token_program.key(), false), // token program
-                    AccountMeta::new_readonly(remaining_accounts[0].key(), false), // program authority
+                    AccountMeta::new_readonly(program_authority.key(), false), // program authority
                     AccountMeta::new_readonly(self.token_account_authority.key(), true), // user transfer authority
                     AccountMeta::new(self.source_token_account.key(), false),       // source token account
-                    AccountMeta::new(remaining_accounts[1].key(), false), // program source token account
-                    AccountMeta::new(remaining_accounts[2].key(), false), // program destination token account
+                    AccountMeta::new(program_source_token_account.key(), false), // program source token account
+                    AccountMeta::new(program_destination_token_account.key(), false), // program destination token account
                     AccountMeta::new(self.destination_token_account.key(), false), // destination token account
                     AccountMeta::new_readonly(self.source_mint.key(), false), // source mint
                     AccountMeta::new_readonly(self.destination_mint.key(), false), // destination mint
@@ -179,6 +251,93 @@ impl<'info> Jupiter<'info> {
                 }));
 
                 msg!("Performing a CPI with SharedAccounts (ExactOutRoute or Route)");
+
+                (account_infos, accounts)
+            }
+            data if data.starts_with(&EXACT_OUT_ROUTE_V2_DISCRIMINATOR)
+                || data.starts_with(&ROUTE_V2_DISCRIMINATOR) =>
+            {
+                if remaining_accounts.len() < 2 {
+                    return Err(Error::from(ProgramError::NotEnoughAccountKeys));
+                }
+
+                let source_token_program = &remaining_accounts[0];
+                let destination_token_program = &remaining_accounts[1];
+
+                let account_infos = vec![
+                    self.token_account_authority.to_account_info(),
+                    self.source_token_account.to_account_info(),
+                    self.destination_token_account.to_account_info(),
+                    self.source_mint.to_account_info(),
+                    self.destination_mint.to_account_info(),
+                    source_token_program.to_account_info(),
+                    destination_token_program.to_account_info(),
+                    self.destination_token_account.to_account_info(),
+                    self.event_authority.to_account_info(),
+                    self.jupiter_program.to_account_info(),
+                ];
+
+                let accounts = vec![
+                    AccountMeta::new_readonly(self.token_account_authority.key(), true), // token account authority
+                    AccountMeta::new(self.source_token_account.key(), false),             // source token account
+                    AccountMeta::new(self.destination_token_account.key(), false), // destination token account
+                    AccountMeta::new_readonly(self.source_mint.key(), false),      // source mint
+                    AccountMeta::new_readonly(self.destination_mint.key(), false), // destination mint
+                    AccountMeta::new_readonly(source_token_program.key(), false),  // source token program
+                    AccountMeta::new_readonly(destination_token_program.key(), false), // destination token program
+                    AccountMeta::new(self.destination_token_account.key(), false), // destination token account (quoted output)
+                    AccountMeta::new_readonly(self.event_authority.key(), false),  // event authority
+                    AccountMeta::new_readonly(self.jupiter_program.key(), false),   // jupiter program
+                ];
+
+                msg!("Performing a CPI with RouteV2 / ExactOutRouteV2");
+
+                (account_infos, accounts)
+            }
+            data if data.starts_with(&SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR)
+                || data.starts_with(&SHARED_ACCOUNTS_ROUTE_V2_DISCRIMINATOR) =>
+            {
+                if remaining_accounts.len() < 5 {
+                    return Err(Error::from(ProgramError::NotEnoughAccountKeys));
+                }
+
+                let program_authority = &remaining_accounts[0];
+                let program_source_token_account = &remaining_accounts[1];
+                let program_destination_token_account = &remaining_accounts[2];
+                let source_token_program = &remaining_accounts[3];
+                let destination_token_program = &remaining_accounts[4];
+
+                let account_infos = vec![
+                    program_authority.to_account_info(),
+                    self.token_account_authority.to_account_info(),
+                    self.source_token_account.to_account_info(),
+                    program_source_token_account.to_account_info(),
+                    program_destination_token_account.to_account_info(),
+                    self.destination_token_account.to_account_info(),
+                    self.source_mint.to_account_info(),
+                    self.destination_mint.to_account_info(),
+                    source_token_program.to_account_info(),
+                    destination_token_program.to_account_info(),
+                    self.event_authority.to_account_info(),
+                    self.jupiter_program.to_account_info(),
+                ];
+
+                let accounts = vec![
+                    AccountMeta::new_readonly(program_authority.key(), false), // program authority
+                    AccountMeta::new_readonly(self.token_account_authority.key(), true), // token account authority
+                    AccountMeta::new(self.source_token_account.key(), false), // source token account
+                    AccountMeta::new(program_source_token_account.key(), false), // program source token account
+                    AccountMeta::new(program_destination_token_account.key(), false), // program destination token account
+                    AccountMeta::new(self.destination_token_account.key(), false), // destination token account
+                    AccountMeta::new_readonly(self.source_mint.key(), false), // source mint
+                    AccountMeta::new_readonly(self.destination_mint.key(), false), // destination mint
+                    AccountMeta::new_readonly(source_token_program.key(), false), // source token program
+                    AccountMeta::new_readonly(destination_token_program.key(), false), // destination token program
+                    AccountMeta::new_readonly(self.event_authority.key(), false), // event authority
+                    AccountMeta::new_readonly(self.jupiter_program.key(), false), // jupiter program
+                ];
+
+                msg!("Performing a CPI with SharedAccounts RouteV2 / ExactOutRouteV2");
 
                 (account_infos, accounts)
             }
--- playground/metis.ts
+++ playground/metis.ts
@@ -3,7 +3,7 @@ import {
 	SwapApi,
 	type QuoteResponse,
 } from "@blueshift/beethoven/metis";
-import { PublicKey, type TransactionInstruction } from "@solana/web3.js";
+import { PublicKey, type TransactionInstruction, ComputeBudgetProgram } from "@solana/web3.js";
 import {
 	arraysEqual,
 	buildTransaction,
@@ -30,6 +30,14 @@ const SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR = [
 const SHARED_ACCOUNTS_ROUTE_DISCRIMINATOR = [
 	193, 32, 155, 51, 65, 214, 156, 129,
 ];
+const EXACT_OUT_ROUTE_V2_DISCRIMINATOR = [157, 138, 184, 82, 21, 244, 243, 36];
+const ROUTE_V2_DISCRIMINATOR = [187, 100, 250, 204, 49, 196, 175, 20];
+const SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR = [
+	53, 96, 229, 202, 216, 187, 250, 24,
+];
+const SHARED_ACCOUNTS_ROUTE_V2_DISCRIMINATOR = [
+	209, 152, 83, 147, 124, 254, 216, 233,
+];
 
 const userPublicKey = keypair.publicKey.toBase58();
 const inputMint = "So11111111111111111111111111111111111111112";
@@ -47,7 +55,7 @@ const metisClient = new SwapApi(
 // bandage fix to convert null percent in routePlan to 100
 function normalizeSingleRoutePercent(quote: QuoteResponse): QuoteResponse {
 	const normalizedRoutePlan = quote.routePlan.map((route) =>
-		route.percent === null ? { ...route, percent: 100 } : route,
+		route.percent === null ? { ...route, percent: route.bps ? Math.floor(route.bps / 100) : 100 } : route,
 	);
 	const changed = normalizedRoutePlan.some(
 		(route, index) => route !== quote.routePlan[index],
@@ -73,9 +81,11 @@ function extractRemainingAccountsForSwap(
 	if (arraysEqual(discriminator, ROUTE_DISCRIMINATOR)) {
 		// For Route, the first 9 accounts are base accounts
 		remainingAccounts = swapInstruction.keys.slice(9);
+		console.log("route");
 	} else if (arraysEqual(discriminator, EXACT_OUT_ROUTE_DISCRIMINATOR)) {
 		// For ExactOutRoute, the first 11 accounts are base accounts
 		remainingAccounts = swapInstruction.keys.slice(11);
+		console.log("exactOutRoute");
 	} else if (
 		arraysEqual(discriminator, SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR) ||
 		arraysEqual(discriminator, SHARED_ACCOUNTS_ROUTE_DISCRIMINATOR)
@@ -97,6 +107,44 @@ function extractRemainingAccountsForSwap(
 			programDestinationTokenAccount,
 			...otherRemainingAccounts,
 		];
+		console.log(arraysEqual(discriminator, SHARED_ACCOUNTS_EXACT_OUT_ROUTE_DISCRIMINATOR)
+			? "sharedAccountsExactOutRoute" : "sharedAccountsRoute");
+	} else if (
+		arraysEqual(discriminator, EXACT_OUT_ROUTE_V2_DISCRIMINATOR) ||
+		arraysEqual(discriminator, ROUTE_V2_DISCRIMINATOR)
+	) {
+		// For RouteV2 / ExactOutRouteV2, Anchor Jupiter expects:
+		// - remaining_accounts[0] = source token program (position 5)
+		// - remaining_accounts[1] = destination token program (position 6)
+		// - remaining_accounts[2+] = all other remaining accounts (position 10+)
+		const sourceTokenProgram = swapInstruction.keys[5];
+		const destinationTokenProgram = swapInstruction.keys[6];
+		const otherRemainingAccounts = swapInstruction.keys.slice(10);
+		
+		remainingAccounts = [sourceTokenProgram, destinationTokenProgram, ...otherRemainingAccounts];
+		console.log(arraysEqual(discriminator, EXACT_OUT_ROUTE_V2_DISCRIMINATOR)
+			? "exactOutRouteV2" : "routeV2");
+	} else if (
+		arraysEqual(discriminator, SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR) ||
+		arraysEqual(discriminator, SHARED_ACCOUNTS_ROUTE_V2_DISCRIMINATOR)
+	) {
+		// For SharedAccounts RouteV2 / ExactOutRouteV2, Anchor Jupiter expects:
+		// - remaining_accounts[0] = program authority (position 0)
+		// - remaining_accounts[1] = program source token account (position 3)
+		// - remaining_accounts[2] = program destination token account (position 4)
+		// - remaining_accounts[3] = source token program (position 8)
+		// - remaining_accounts[4] = destination token program (position 9)
+		// - remaining_accounts[5+] = all other remaining accounts (position 12+)
+		const programAuthority = swapInstruction.keys[0];
+		const programSourceTokenAccount = swapInstruction.keys[3];
+		const programDestinationTokenAccount = swapInstruction.keys[4];
+		const sourceTokenProgram = swapInstruction.keys[8];
+		const destinationTokenProgram = swapInstruction.keys[9];
+		const otherRemainingAccounts = swapInstruction.keys.slice(12);
+		
+		remainingAccounts = [programAuthority, programSourceTokenAccount, programDestinationTokenAccount, sourceTokenProgram, destinationTokenProgram, ...otherRemainingAccounts];
+		console.log(arraysEqual(discriminator, SHARED_ACCOUNTS_EXACT_OUT_ROUTE_V2_DISCRIMINATOR)
+			? "sharedAccountsExactOutRouteV2" : "sharedAccountsRouteV2");
 	} else {
 		throw new Error(`Unknown discriminator: ${discriminator}`);
 	}
@@ -113,7 +161,7 @@ async function main() {
 		amount,
 		slippageBps,
 		// V2 requires bandage fix, V1 does not
-		instructionVersion: "V1",
+		instructionVersion: "V2",
 	});
 
 	const instructions = await metisClient.swapInstructionsPost({
@@ -129,7 +177,9 @@ async function main() {
 	console.log("quote.outAmount:", quote.outAmount);
 
 	const ixs: TransactionInstruction[] = [
-		...instructions.computeBudgetInstructions.map(deserializeInstruction),
+		// ...instructions.computeBudgetInstructions.map(deserializeInstruction),
+		ComputeBudgetProgram.setComputeUnitLimit({ units: 400_000 }),
+		ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 5000 }),
 		...instructions.setupInstructions.map(deserializeInstruction),
 	];
 
@@ -170,6 +220,7 @@ async function main() {
 		),
 	});
 
+	// console.log(Buffer.from(tx.serialize()).toString("base64"));
 	const sig = await sendTransaction(tx);
 	console.log("sig:", sig);
 }
